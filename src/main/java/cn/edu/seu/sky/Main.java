7f5655257aff6a7bf9e23e82c025472262bbd290
xiaotian.zhou <xiaotian.zhou@amh-group.com>
2021-04-29 10:53:20 +0800

    init

---
+++ src/main/java/cn/edu/seu/sky/Main.java
@@ -0,0 +1,12 @@
+package cn.edu.seu.sky;
+
+/**
+ * @author xiaotian.zhou
+ * @date 2021/4/29 10:52
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
