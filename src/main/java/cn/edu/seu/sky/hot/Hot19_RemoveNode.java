44c7c490c8057b23d2be4c36e3f4f975efacb799
xiaotian <xiao.tian@vip.163.com>
2022-06-17 10:55:20 +0800

    feat: remove node

--- src/main/java/cn/edu/seu/sky/hot/Hot19_RemoveNode.java
+++ src/main/java/cn/edu/seu/sky/hot/Hot19_RemoveNode.java
@@ -9,10 +9,31 @@ import cn.edu.seu.sky.common.ListNode;
 public class Hot19_RemoveNode {
 
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        return null;
+        if (head == null) {
+            return null;
+        }
+        ListNode p = head, q = head;
+        while (n-- > 0) {
+            p = p.next;
+        }
+        if (p == null) {
+            return head.next;
+        }
+        while (p.next != null) {
+            p = p.next;
+            q = q.next;
+        }
+        q.next = q.next.next;
+        return head;
     }
 
     public static void main(String[] args) {
+        Hot19_RemoveNode func = new Hot19_RemoveNode();
 
+        ListNode node = ListNode.create(new int[]{1, 2, 3, 4, 5}).print();
+        func.removeNthFromEnd(node, 2).print();
+
+        ListNode node1 = ListNode.create(new int[]{1}).print();
+        System.out.println(func.removeNthFromEnd(node1, 1) == null);
     }
 }
